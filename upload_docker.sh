161933e500ea9f54ad523dbaabb3c60bba9b9ded
Hudeyfa Yusuf <hudeyfa.md@gmail.com>
2022-09-04 15:45:53 +0000

    Complete the upload_docker.sh file

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath="hudeyfa/sklearn-model-api"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker logi
+docker tag sklearn-model-api $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
